25650ac92640450f73598f8cecc952390da1c7b5
sintaanjelina <sinta.anjelina3930@gmail.com>
2022-06-09 12:26:20 +0700

    call updateScreen function when number is clicked

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const numbers = document.querySelectorAll(".number");
-
-numbers.forEach((number) => {
-    console.log(number);
-    number.addEventListener("click", (event) => {
-        console.log(event.target.value, "number is pressed");
-    })
-})
-
 const calculatorScreen = document.querySelector(".calculator-screen");
 
 const updateScreen = (number) => {
     calculatorScreen.value = number;
 }
+
+numbers.forEach((number) => {
+    console.log(number);
+    number.addEventListener("click", (event) => {
+        updateScreen(event.target.value);
+    })
+})
+
+
