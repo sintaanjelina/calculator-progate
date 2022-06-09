e46eb96eb05cc174998676e7566f5f2d3e166cfa
sintaanjelina <sinta.anjelina3930@gmail.com>
2022-06-09 13:04:55 +0700

    Save clicked number value into currentNumber variable

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const numbers = document.querySelectorAll(".number");
 const calculatorScreen = document.querySelector(".calculator-screen");
 
+let prevNumber = '';
+let calculationOperator = '';
+let currentNumber = '0';
+
 const updateScreen = (number) => {
     calculatorScreen.value = number;
+}
+
+const inputNumber = (number) => {
+    currentNumber = number;
 }
 
 numbers.forEach((number) => {
     console.log(number);
     number.addEventListener("click", (event) => {
+        inputNumber(event.target.value);
         updateScreen(event.target.value);
     })
 })
-
-let prevNumber = '';
-let calculationOperator = '';
-let currentNumber = '0';
-
