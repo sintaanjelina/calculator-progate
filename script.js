b7a10a5a6362e67da26d7aa0986ba634e77facae
sintaanjelina <sinta.anjelina3930@gmail.com>
2022-06-13 21:02:14 +0700

    Additional modification for btn percentage, clearone, and calculate function

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ const operators = document.querySelectorAll(".operator");
 const equalSign = document.querySelector(".equal-sign");
 const clearBtn = document.querySelector(".all-clear");
 const decimal = document.querySelector(".decimal");
+const calculatorHistory = document.querySelector(".calculator-history");
+const percentageBtn = document.querySelector(".percentage");
+const clearOneBtn = document.querySelector(".one-clear");
 
 let prevNumber = '';
 let calculationOperator = '';
 let currentNumber = '0';
+let calculationHistory = '';
+let percentNumber = '';
 
 const inputDecimal = (dot) => {
     if (currentNumber.includes('.')) {
@@ -19,11 +24,36 @@ const inputDecimal = (dot) => {
 const clearAll = () => {
     prevNumber = '';
     calculationOperator = '';
+    // calculationHistory = '';
     currentNumber = '0';
 }
 
+const clearOne = () => {
+    if (!isNaN(currentNumber)) {
+        currentNumber = currentNumber.toString().slice(0, -1);
+        if (currentNumber == "") {
+            currentNumber = '0';
+        }
+    } else if (!isNaN(prevNumber)) {
+        currentNumber = prevNumber;
+        prevNumber = "";
+        currentNumber = currentNumber.toString().slice(0, -1);
+    }
+}
+
+const percent = (number) => {
+    currentNumber = parseFloat(number) / 100;
+}
+
 const calculate = () => {
+    console.log("before calculate prev", prevNumber, "current", currentNumber, calculationOperator);
     let result = '';
+    if (isNaN(parseFloat(prevNumber)) || isNaN(parseFloat(currentNumber))) {
+        currentNumber = prevNumber + currentNumber;
+        prevNumber = "";
+        calculationOperator = "";
+        return;
+    }
     switch (calculationOperator) {
         case '+':
             result = parseFloat(prevNumber) + parseFloat(currentNumber);
@@ -33,6 +63,7 @@ const calculate = () => {
             break;
         case '*':
             result = parseFloat(prevNumber) * parseFloat(currentNumber);
+            console.log(result);
             break;
         case '/':
             result = parseFloat(prevNumber) / parseFloat(currentNumber);
@@ -42,24 +73,45 @@ const calculate = () => {
     }
     currentNumber = result;
     calculationOperator = "";
+    prevNumber = "";
 }
 
 const inputOperator = (operator) => {
+    console.log("input prev", parseFloat(prevNumber), "current", parseFloat(currentNumber), calculationOperator);
+
     if (calculationOperator == "") {
         prevNumber = currentNumber;
     }
+    if (!isNaN(parseFloat(prevNumber)) && !isNaN(parseFloat(currentNumber)) && calculationOperator !== "") {
+        console.log("before input operator prev", parseFloat(prevNumber), "current", parseFloat(currentNumber), calculationOperator, (isNaN(parseFloat(prevNumber)) || isNaN(parseFloat(currentNumber))) && calculationOperator !== "");
+        calculate();
+        prevNumber = currentNumber;
+        console.log("after input operator prev", parseFloat(prevNumber), "current", parseFloat(currentNumber), calculationOperator, (isNaN(parseFloat(prevNumber)) || isNaN(parseFloat(currentNumber))) && calculationOperator !== "");
+    }
     calculationOperator = operator;
+    console.log("input operator check prev", prevNumber, "current", currentNumber, calculationOperator);
     currentNumber = "";
 }
 
 operators.forEach((operator) => {
     operator.addEventListener("click", (event) => {
+        // updateHistory(event.target.value);
         inputOperator(event.target.value);
     })
 })
 
 const updateScreen = (number) => {
     calculatorScreen.value = number;
+}
+
+const updateHistory = (number) => {
+    calculationHistory += number;
+    if (calculationHistory === "") {
+        calculatorHistory.value = number;
+    }
+    else {
+        calculatorHistory.value = calculationHistory;
+    }
 }
 
 const inputNumber = (number) => {
@@ -73,6 +125,7 @@ const inputNumber = (number) => {
 numbers.forEach((number) => {
     number.addEventListener("click", (event) => {
         inputNumber(event.target.value);
+        // updateHistory(event.target.value);
         updateScreen(currentNumber);
     })
 })
@@ -80,15 +133,29 @@ numbers.forEach((number) => {
 clearBtn.addEventListener('click', () => {
     clearAll();
     updateScreen(currentNumber);
+    // updateHistory(currentNumber);
+    clearAll();
 })
 
-equalSign.addEventListener('click', () => {
+equalSign.addEventListener('click', (event) => {
     calculate();
+    console.log(currentNumber);
     updateScreen(currentNumber);
-    clearAll();
+    // updateHistory(` = ${currentNumber}`);
 })
 
 decimal.addEventListener('click', (event) => {
     inputDecimal(event.target.value);
+    updateScreen(currentNumber);
+    // updateHistory(currentNumber);
+})
+
+percentageBtn.addEventListener('click', () => {
+    percent(currentNumber);
+    updateScreen(currentNumber);
+})
+
+clearOneBtn.addEventListener('click', () => {
+    clearOne();
     updateScreen(currentNumber);
 })
